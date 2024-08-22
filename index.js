8db2658a3d48e1c7741d7e15268a158f74c9ab5f
aknlite48 <aanandk2705@gmail.com>
2024-08-22 18:54:20 -0400

    final prod with mongodb

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Contact = require('./models/contact')
 const morgan = require('morgan')
 
 app.use(express.json())
@@ -17,31 +19,52 @@ app.use(morgan(function (tokens, req, res) {
       (tokens.method(req,res)==='POST') ? JSON.stringify(req.body) : ''
     ].join(' ')
   }))
+
+//comment out for prod code
 app.use(express.static('dist'))
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.post('/api/persons',(request,response)=>{
+    const new_person = request.body
+    if (!new_person.name) {
+        response.status(400).json({error:"name missing"})
+        return
+    }
+    if (!new_person.number) {
+        response.status(400).json({error:"number missing"})
+        return
+    }
+    if (isNaN(new_person.number)) {
+        response.status(400).json({error:"invalid number"})
+        return
+    }
+
+    let name_duplicate=false
+    /*
+    Contact.find({})
+    .then((result)=>{
+        for (let p of result) {
+            if (p.name===new_person.name) {
+                name_duplicate=true
+            }
+        }
+    })*/
+
+    const new_p = new Contact({
+        name: new_person.name,
+        number: new_person.number
+    })
+
+    new_p.save()
+    .then((result)=>{
+        response.json(result)
+    })
+    
+    
+
+
+})
+
+/*app.post('/api/persons',(request,response)=>{
     const new_person = request.body
 
     if (!new_person.name) {
@@ -74,52 +97,63 @@ app.post('/api/persons',(request,response)=>{
     response.json(new_p)
 
 
-})
+})*/
 
 app.get('/api/persons',(request,response)=>{
-    response.json(persons)
+    Contact.find({})
+    .then((result)=>{
+        response.json(result)
+    })
 })
 
+
+/*
 app.get('/api/info',(request,response)=>{
     let disp = `<p>Phonebook has info for ${persons.length} persons</p>`
     const now = new Date()
     disp = disp+`<p>${now}</p>`
     response.send(disp)
 })
+*/
 
+//get by id
 app.get('/api/persons/:id',(request,response)=>{
-    const id = request.params.id
-    const person = persons.find((n)=>{return n.id===id})
-    if (person) {
-        response.json(person)
-    }
-    else {
-        response.status(404).end()
-    }
+    Contact.findById(request.params.id)
+    .then((result)=>{
+        response.json(result)
+    })
+    .catch(()=>{
+        response.status(404).json({error: "not found"})
+    })
+})
+
+//get by name
+app.get('/api/persons/name/:name',(request,response)=>{
+    Contact.find({name:request.params.name})
+    .then((result)=>{
+        response.json(result)
+    })
 })
 
 app.put('/api/persons/:id',(request,response)=>{
-    const new_p = request.body
+    const new_p = {
+        name: request.body.content,
+        number: request.body.number
+    }
     const id = request.params.id
-    persons = persons.map(
-        (n)=>{
-            if (n.id!==id) {
-                return n
-            }
-            else {
-                return {id:id,...new_p}
-            }
-        }
-    )
-    response.json({id:id,...new_p})
+    Contact.findByIdAndUpdate(id,new_p,{new:true})
+    .then((result)=>{response.json(result)})
+    .catch(()=>{response.status(404).end()})
 })
+
 
 app.delete('/api/persons/:id',(request,response)=>{
     const id = request.params.id
-    persons = persons.filter((n)=>{return n.id!==id})
-    //response.json(request)
-    response.status(204).end()
+    Contact.findByIdAndDelete(id)
+    .then(()=>{response.status(204).end()})
+    .catch(()=>{response.status(404).end()})
 })
+
 
 PORT = process.env.PORT || 3001
 app.listen(PORT)
